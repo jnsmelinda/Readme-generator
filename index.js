a42cfb8e9f71093af4f45b3984aa729496e208a5
Melinda Ivanov <jns.melinda@gmail.com>
2020-08-11 17:35:09 -0700

    import generateMarkdown po index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // array of questions for user
 const questions = [
 
@@ -9,7 +14,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    console.log(generateMarkdown({title: 'asdfghjkl'}));
 }
 
 // function call to initialize program
